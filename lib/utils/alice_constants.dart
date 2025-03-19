9d75432a5a1eb79665de068689cc9ee114449cab
ferdiangunawan <ferdiangunawan98@gmail.com>
2025-03-19 13:56:46 +0700

    HRSIB-10121 - Alice Improvement, add payload menu and revamp the alice log response

--- lib/utils/alice_constants.dart
+++ lib/utils/alice_constants.dart
@@ -4,6 +4,7 @@ class AliceConstants {
   static Color red = Color(0xffff3f34);
   static Color lightRed = Color(0xffff5e57);
   static Color green = Color(0xff05c46b);
+  static Color blue = Colors.lightBlue;
   static Color grey = Color(0xff808e9b);
   static Color orange = Color(0xffffa801);
 }
